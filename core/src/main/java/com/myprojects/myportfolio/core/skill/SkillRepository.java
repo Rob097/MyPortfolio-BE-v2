61e4697069aa4a26b07802ddf090a0cff83c563e
Roberto dellantonio <roberto.dellantonio@fleap.io>
2023-10-09 17:47:31 +0200

    WIP Core Refactor

--- core/src/main/java/com/myprojects/myportfolio/core/skill/SkillRepository.java
+++
@@ -1,9 +0,0 @@
-package com.myprojects.myportfolio.core.skill;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface SkillRepository extends JpaRepository<Skill, Integer>, JpaSpecificationExecutor<Skill> {
-}
--- core/src/main/java/com/myprojects/myportfolio/core/skill/SkillsCategoryRepository.java
+++
@@ -1,9 +0,0 @@
-package com.myprojects.myportfolio.core.skill;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface SkillsCategoryRepository extends JpaRepository<SkillsCategory, Integer>, JpaSpecificationExecutor<SkillsCategory> {
-}
