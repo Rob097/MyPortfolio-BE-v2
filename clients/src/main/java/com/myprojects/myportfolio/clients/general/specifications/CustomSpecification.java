0d5f918b2aa7ea3b6e5d0d4835c6d4cbfdd849e7
Roberto dellantonio <roberto.dellantonio@fleap.io>
2023-11-15 18:03:10 +0100

    Improved filters in OR and LIKE

--- clients/src/main/java/com/myprojects/myportfolio/clients/general/specifications/CustomSpecification.java
+++ clients/src/main/java/com/myprojects/myportfolio/clients/general/specifications/CustomSpecification.java
@@ -1,15 +1,17 @@
 package com.myprojects.myportfolio.clients.general.specifications;
 
+import jakarta.persistence.criteria.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.data.jpa.domain.Specification;
 
-import jakarta.persistence.criteria.*;
 import java.text.ParseException;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 @Data
 @NoArgsConstructor
@@ -22,112 +24,136 @@ public class CustomSpecification<T> implements Specification<T> {
     public Predicate toPredicate(Root<T> root, CriteriaQuery<?> query, CriteriaBuilder criteriaBuilder) {
         try {
 
-            Expression<T> rootExpression;
+            // If the key contains a space, we need to apply the filters to all the fields
+            String[] fields = criteria.getKey().split("\\s+");
 
-            if (criteria.getKey().contains(".")) {
-                String childTable = criteria.getKey().substring(0, criteria.getKey().indexOf("."));
-                String childField = criteria.getKey().substring(criteria.getKey().indexOf(".") + 1);
-                rootExpression = colJoin(root, childTable).get(childField);
-            } else {
-                rootExpression = root.get(criteria.getKey());
+            List<Predicate> orPredicates = new ArrayList<>();
+            for (String field : fields) {
+                orPredicates.add(applyFiltersToField(root, criteriaBuilder, field));
             }
 
-            // greaterThanOrEqualTo
-            if (criteria.getOperation().equalsIgnoreCase(">")) {
+            return criteriaBuilder.or(orPredicates.toArray(new Predicate[0]));
 
-                // LocalDateTime
-                if (rootExpression.getJavaType() == LocalDateTime.class) {
-                    return this.checkDates(root, criteriaBuilder, criteria.getOperation());
-
-                    // EveryThing else
-                } else {
-                    return criteriaBuilder.greaterThanOrEqualTo(
-                            rootExpression.as(String.class), criteria.getValue().toString());
-                }
-            }
-
-            // lessThanOrEqualTo
-            else if (criteria.getOperation().equalsIgnoreCase("<")) {
-
-                // LocalDateTime
-                if (rootExpression.getJavaType() == LocalDateTime.class) {
-                    return this.checkDates(root, criteriaBuilder, criteria.getOperation());
-
-                    // EveryThing else
-                } else {
-                    return criteriaBuilder.lessThanOrEqualTo(
-                            rootExpression.as(String.class), criteria.getValue().toString());
-                }
-            }
-
-            // equal
-            else if (criteria.getOperation().equalsIgnoreCase(":")) {
-
-                // String in like
-                if (rootExpression.getJavaType() == String.class) {
-
-                    String newValue = ((String) criteria.getValue()).replace("*", "%");
-                    return criteriaBuilder.like(rootExpression.as(String.class), newValue);
-
-                    // LocalDateTime
-                } else if (rootExpression.getJavaType() == LocalDateTime.class) {
-                    return this.checkDates(root, criteriaBuilder, criteria.getOperation());
-
-                    // Boolean
-                } else if (rootExpression.getJavaType() == Boolean.class) {
-                    if (criteria.getValue().equals("true")) {
-                        return criteriaBuilder.isTrue(rootExpression.as(Boolean.class));
-                    } else {
-                        return criteriaBuilder.isFalse(rootExpression.as(Boolean.class));
-                    }
-
-                    // EveryThing else
-                } else {
-                    return criteriaBuilder.equal(rootExpression, criteria.getValue());
-                }
-            }
-
-            // notEqual
-            else if (criteria.getOperation().equalsIgnoreCase("!")) {
-
-                // String in like
-                if (rootExpression.getJavaType() == String.class) {
-
-                    String newValue = ((String) criteria.getValue()).replace("*", "%");
-                    return criteriaBuilder.notLike(rootExpression.as(String.class), newValue);
-
-                    // LocalDateTime
-                } else if (rootExpression.getJavaType() == LocalDateTime.class) {
-                    return this.checkDates(root, criteriaBuilder, criteria.getOperation());
-
-                    // Boolean
-                } else if (rootExpression.getJavaType() == Boolean.class) {
-                    if (criteria.getValue().equals("true")) {
-                        return criteriaBuilder.isFalse(rootExpression.as(Boolean.class));
-                    } else {
-                        return criteriaBuilder.isTrue(rootExpression.as(Boolean.class));
-                    }
-
-                    // EveryThing else
-                } else {
-                    return criteriaBuilder.notEqual(rootExpression, criteria.getValue());
-                }
-            }
-
-
-            return null;
         } catch (ParseException e) {
             throw new RuntimeException(e);
         }
     }
 
-    /*public Specification<T> equalsToSpecification(@NonNull String field, @NonNull String value) {
-        return (root, query, criteriaBuilder) -> criteriaBuilder.equal(root.get(field), value);
-    }*/
-
     /**********************/
     /*** Private Methods **/
     /**********************/
+
+    private Predicate applyFiltersToField(Root<T> root, CriteriaBuilder criteriaBuilder, String field) throws ParseException {
+        Expression<T> rootExpression;
+
+        if (field.contains(".")) {
+            String childTable = field.substring(0, field.indexOf("."));
+            String childField = field.substring(field.indexOf(".") + 1);
+            rootExpression = colJoin(root, childTable).get(childField);
+        } else {
+            rootExpression = root.get(field);
+        }
+
+        // greaterThanOrEqualTo
+        if (criteria.getOperation().equalsIgnoreCase(">")) {
+
+            // LocalDateTime
+            if (rootExpression.getJavaType() == LocalDateTime.class) {
+                return this.checkDates(root, criteriaBuilder, criteria.getOperation());
+
+                // EveryThing else
+            } else {
+                return criteriaBuilder.greaterThanOrEqualTo(
+                        rootExpression.as(String.class), criteria.getValue().toString());
+            }
+        }
+
+        // lessThanOrEqualTo
+        else if (criteria.getOperation().equalsIgnoreCase("<")) {
+
+            // LocalDateTime
+            if (rootExpression.getJavaType() == LocalDateTime.class) {
+                return this.checkDates(root, criteriaBuilder, criteria.getOperation());
+
+                // EveryThing else
+            } else {
+                return criteriaBuilder.lessThanOrEqualTo(
+                        rootExpression.as(String.class), criteria.getValue().toString());
+            }
+        }
+
+        // equal
+        else if (criteria.getOperation().equalsIgnoreCase(":")) {
+
+            // String in like
+            if (rootExpression.getJavaType() == String.class) {
+
+                String[] valueParts = ((String) criteria.getValue()).split("\\*");
+                List<Predicate> orPredicates = new ArrayList<>();
+                for (String valuePart : valueParts) {
+                    if (!valuePart.isEmpty()) {
+                        orPredicates.add(criteriaBuilder.like(rootExpression.as(String.class), "%" + valuePart + "%"));
+                    }
+                }
+
+                return criteriaBuilder.or(orPredicates.toArray(new Predicate[0]));
+
+                // LocalDateTime
+            } else if (rootExpression.getJavaType() == LocalDateTime.class) {
+                return this.checkDates(root, criteriaBuilder, criteria.getOperation());
+
+                // Boolean
+            } else if (rootExpression.getJavaType() == Boolean.class) {
+                if (criteria.getValue().equals("true")) {
+                    return criteriaBuilder.isTrue(rootExpression.as(Boolean.class));
+                } else {
+                    return criteriaBuilder.isFalse(rootExpression.as(Boolean.class));
+                }
+
+                // EveryThing else
+            } else {
+                return criteriaBuilder.equal(rootExpression, criteria.getValue());
+            }
+        }
+
+        // notEqual
+        else if (criteria.getOperation().equalsIgnoreCase("!")) {
+
+            // String in like
+            if (rootExpression.getJavaType() == String.class) {
+
+                String[] valueParts = ((String) criteria.getValue()).split("\\*");
+                List<Predicate> orPredicates = new ArrayList<>();
+                for (String valuePart : valueParts) {
+                    if (!valuePart.isEmpty()) {
+                        orPredicates.add(criteriaBuilder.notLike(rootExpression.as(String.class), "%" + valuePart + "%"));
+                    }
+                }
+
+                return criteriaBuilder.or(orPredicates.toArray(new Predicate[0]));
+
+                // LocalDateTime
+            } else if (rootExpression.getJavaType() == LocalDateTime.class) {
+                return this.checkDates(root, criteriaBuilder, criteria.getOperation());
+
+                // Boolean
+            } else if (rootExpression.getJavaType() == Boolean.class) {
+                if (criteria.getValue().equals("true")) {
+                    return criteriaBuilder.isFalse(rootExpression.as(Boolean.class));
+                } else {
+                    return criteriaBuilder.isTrue(rootExpression.as(Boolean.class));
+                }
+
+                // EveryThing else
+            } else {
+                return criteriaBuilder.notEqual(rootExpression, criteria.getValue());
+            }
+        }
+
+
+        return null;
+    }
+
     private Predicate checkDates(Expression<?> rootExpression, CriteriaBuilder criteriaBuilder, String operation) throws ParseException {
         LocalDateTime localDateTime = (LocalDateTime) criteria.getValue();
         Date date = Date.from(localDateTime.atZone(ZoneId.systemDefault()).toInstant());
