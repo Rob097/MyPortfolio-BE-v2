527df58eb1c4621af3803095b8145058fa827051
Roberto dellantonio <roberto.dellantonio@fleap.io>
2024-05-16 16:37:15 +0200

    If authorization header is present, passed user even if is an OpenAPI

--- apigw/src/main/java/com/myprojects/myportfolio/apigw/config/AuthenticationFilter.java
+++ apigw/src/main/java/com/myprojects/myportfolio/apigw/config/AuthenticationFilter.java
@@ -35,44 +35,46 @@ public class AuthenticationFilter implements GatewayFilter {
     public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
 
         org.springframework.cloud.gateway.route.Route route = exchange.getAttribute(org.springframework.cloud.gateway.support.ServerWebExchangeUtils.GATEWAY_ROUTE_ATTR);
-        if(route!=null)
+        if (route != null)
             log.info("Incoming request for " + route.getId());
 
-        if (!routerValidator.isHttpServletRequestSecured.test(exchange.getRequest())) {
-            long startTime = System.currentTimeMillis();
-            log.info("Authenticating request");
+        boolean isOpenAPI = routerValidator.isHttpServletRequestSecured.test(exchange.getRequest());
 
-            if (!exchange.getRequest().getHeaders().containsKey(HttpHeaders.AUTHORIZATION)) {
+        long startTime = System.currentTimeMillis();
+        log.info("Authenticating request");
+
+        if (!exchange.getRequest().getHeaders().containsKey(HttpHeaders.AUTHORIZATION)) {
+            if (!isOpenAPI) {
                 throw new UnauthorizedException("Missing Authorization header");
+            } else {
+                log.info("Request authentication not required");
+                return chain.filter(exchange);
             }
-
-            String token = Objects.requireNonNull(exchange.getRequest().getHeaders().get(HttpHeaders.AUTHORIZATION)).get(0);
-            if (token != null && token.startsWith(jwtConfig.getTokenPrefix())) {
-                token = token.replace(jwtConfig.getTokenPrefix(), "");
-            }
-
-            return authClient.getAuthenticatedUserClaims(token)
-                    .flatMap(responseEntity -> {
-                        log.info("Request authenticated in " + (System.currentTimeMillis() - startTime) + "ms");
-
-                        // Stringify responseEntity as json and set it to the header
-                        exchange.getRequest().mutate().headers(httpHeaders ->
-                                httpHeaders.add(jwtConfig.getAuthenticatedUserClaimsHeader(), responseEntity.toString())
-                        );
-
-                        return chain.filter(exchange);
-                    })
-                    .onErrorResume(throwable -> {
-                        log.warn("Request authentication failed in " + (System.currentTimeMillis() - startTime) + "ms");
-                        ResponseStatusException ex = new ResponseStatusException(HttpStatus.UNAUTHORIZED, "You are unauthorized to access this resource.");
-                        return Mono.error(ex);
-
-                    });
-        } else {
-            log.info("Request authentication not required");
         }
 
-        return chain.filter(exchange);
+        String token = Objects.requireNonNull(exchange.getRequest().getHeaders().get(HttpHeaders.AUTHORIZATION)).get(0);
+        if (token != null && token.startsWith(jwtConfig.getTokenPrefix())) {
+            token = token.replace(jwtConfig.getTokenPrefix(), "");
+        }
+
+        return authClient.getAuthenticatedUserClaims(token)
+                .flatMap(responseEntity -> {
+                    log.info("Request authenticated in " + (System.currentTimeMillis() - startTime) + "ms");
+
+                    // Stringify responseEntity as json and set it to the header
+                    exchange.getRequest().mutate().headers(httpHeaders ->
+                            httpHeaders.add(jwtConfig.getAuthenticatedUserClaimsHeader(), responseEntity.toString())
+                    );
+
+                    return chain.filter(exchange);
+                })
+                .onErrorResume(throwable -> {
+                    log.warn("Request authentication failed in " + (System.currentTimeMillis() - startTime) + "ms");
+                    ResponseStatusException ex = new ResponseStatusException(HttpStatus.UNAUTHORIZED, "You are unauthorized to access this resource.");
+                    return Mono.error(ex);
+
+                });
+
     }
 
 }
