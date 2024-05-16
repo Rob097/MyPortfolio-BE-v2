1f88dfe29e77fc8110b036476514ec8d8fa5b487
Roberto dellantonio <roberto.dellantonio@fleap.io>
2024-05-16 16:38:01 +0200

    If logged user, find returns only user entities

--- core/src/main/java/com/myprojects/myportfolio/core/controllers/UserRelatedBaseController.java
+++ core/src/main/java/com/myprojects/myportfolio/core/controllers/UserRelatedBaseController.java
@@ -1,21 +1,34 @@
 package com.myprojects.myportfolio.core.controllers;
 
 import com.myprojects.myportfolio.clients.general.messages.MessageResource;
+import com.myprojects.myportfolio.clients.general.messages.MessageResources;
+import com.myprojects.myportfolio.clients.general.views.IView;
+import com.myprojects.myportfolio.clients.general.views.Normal;
 import com.myprojects.myportfolio.clients.general.views.Verbose;
 import com.myprojects.myportfolio.core.configAndUtils.UtilsServiceI;
 import com.myprojects.myportfolio.core.dao.BaseDao;
+import com.myprojects.myportfolio.core.dao.Project;
+import com.myprojects.myportfolio.core.dao.Story;
+import com.myprojects.myportfolio.core.dao.User;
 import com.myprojects.myportfolio.core.dto.BaseDto;
+import com.myprojects.myportfolio.core.dto.ProjectDto;
 import com.myprojects.myportfolio.core.dto.groups.OnCreate;
 import com.myprojects.myportfolio.core.dto.groups.OnUpdate;
 import com.myprojects.myportfolio.core.mappers.BaseMapper;
 import com.myprojects.myportfolio.core.services.BaseServiceI;
+import com.myprojects.myportfolio.core.services.StoryServiceI;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang.Validate;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
+@Slf4j
 public abstract class UserRelatedBaseController<A extends BaseDao, T extends BaseDto> extends BaseController<A, T> {
 
     private final BaseServiceI<A> service;
@@ -30,6 +43,34 @@ public abstract class UserRelatedBaseController<A extends BaseDao, T extends Bas
 
     @Autowired
     protected UtilsServiceI utilsService;
+
+    @Override
+    @GetMapping()
+    public ResponseEntity<MessageResources<T>> find(
+            @RequestParam(name = FILTERS, required = false) String filters,
+            @RequestParam(name = "view", required = false, defaultValue = Normal.name) IView view,
+            Pageable pageable
+    ) {
+        Specification<A> specifications = this.defineFilters(filters);
+
+        // If user is logged in, get only the projects of the logged-in user
+        User currentLoggedInUser = utilsService.getCurrentLoggedInUser();
+        if(currentLoggedInUser!=null && (filters==null || !filters.contains("user.id"))) {
+            String key = service instanceof StoryServiceI ? "diary.user.id" : "user.id";
+            log.info("Adding user filter: {}: {}", key, currentLoggedInUser.getId());
+
+            Specification<A> userFilter = findByEquals(key, currentLoggedInUser.getId());
+            if(specifications!=null) {
+                specifications = specifications.and(userFilter);
+            } else {
+                specifications = userFilter;
+            }
+        }
+
+        Page<A> entities = service.findAll(specifications, pageable);
+
+        return this.buildSuccessResponses(entities.map((entity) -> mapper.mapToDto(entity, view)), view);
+    }
 
     /**
      * Here are overridden all the methods that needs a specific "Pre Authorize" condition for all the entities that are related to the current user.
